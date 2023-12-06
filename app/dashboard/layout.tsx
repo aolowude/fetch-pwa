3d9f3f2b37b009062adb445ddba22ce77df1290f
aolowude <aolowude@gmail.com>
2023-12-06 16:04:37 +0000

    Nav to top

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import SideNav from "app/ui/dashboard/sidenav";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="bg-white w-full flex-none md:w-64 absolute bottom-0 md:relative">
+      <div className="bg-white w-full flex-none md:w-64 md:relative">
         <SideNav />
       </div>
       <div className="flex-grow md:overflow-y-auto bg-stone p-7">
